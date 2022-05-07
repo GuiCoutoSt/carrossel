2508be0a8a2c6b084bc3921b09e5dabeb88ed932
Guilherme Couto <gcoutos95@gmail.com>
2022-05-07 19:07:01 -0300

    refact: Refactoring the code

--- src/assets/script.js
+++ src/assets/script.js
@@ -1,30 +1,29 @@
-// Selecionar botões
 const previewButton = document.getElementById("preview");
 const nextButton = document.getElementById("next");
 
 const carouselContent = document.querySelector(".carousel__content");
 
-nextButton.addEventListener("click", () => {
-  scrollAmount = 0;
-
-  let slideTimer = setInterval(() => {
-    carouselContent.scrollLeft += 10;
-    scrollAmount += 10;
-
-    if (scrollAmount >= 300) {
-      clearInterval(slideTimer);
-    }
-  }, 5);
-});
-
 previewButton.addEventListener("click", () => {
-  scrollAmount = 0;
+  scrollMeasure = 0;
 
   let slideTimer = setInterval(() => {
     carouselContent.scrollLeft -= 10;
-    scrollAmount += 10;
+    scrollMeasure += 10;
 
-    if (scrollAmount >= 300) {
+    if (scrollMeasure >= 325) {
+      clearInterval(slideTimer);
+    }
+  }, 10);
+});
+
+nextButton.addEventListener("click", () => {
+  scrollMeasure = 0;
+
+  let slideTimer = setInterval(() => {
+    carouselContent.scrollLeft += 10;
+    scrollMeasure += 10;
+
+    if (scrollMeasure >= 325) {
       clearInterval(slideTimer);
     }
   }, 10);
